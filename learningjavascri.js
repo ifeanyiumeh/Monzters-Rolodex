ede8974200c4f96e9c83d002056a04b56e47555a
Ifeanyi Umeh <59745607+ifeanyiumeh@users.noreply.github.com>
2022-02-18 10:30:25 -0500

    Add files via upload

---
+++ learningjavascri.js
@@ -0,0 +1,13 @@
+const myPromise = new Promise((resolve, reject) => {
+  if (false) {
+    setTimeout(() => {
+      resolve("I have succeeded");
+    }, 1000);
+  } else {
+    reject("I have Failed");
+  }
+});
+
+myPromise
+  .then((value) => console.log(value))
+  .catch((rejectValue) => console.log(rejectValue));
